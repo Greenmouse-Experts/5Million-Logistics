7cd1a702dffccd38b17e32cbc26bb7ce0ad70e7d
Promise <promiseezema11@gmail.com>
2022-12-13 17:12:49 +0100

    updated

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'account_type' => 'Administrator',
+            'name' => 'Admin',
+            'sex' => 'Male',
+            'email' => 'promiseezema11@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('Password')
+        ]);
+    }
+}
